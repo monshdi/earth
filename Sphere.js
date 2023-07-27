9ff90234dbe98fdc2db78d3611d1145ea8c463ee
monshdi <dimamonch1@gmail.com>
2023-07-27 22:38:48 +0700

    start new code for learning threejs

--- Sphere.js
+++ Sphere.js
@@ -1,72 +1,72 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import TextureJPG from './Earth_Diffuse.jpg';
+import { OrbitControls} from "three/addons/controls/OrbitControls.js";
 
-const points = [
-  [44.8439, 132.9179],
-  [59.6644, 28.2792],
-  [46.0962, 39.0745],
-];
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGL1Renderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-const texture = new THREE.TextureLoader().load(TextureJPG);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-const geometry = new THREE.SphereGeometry(5, 32, 32);
-const material = new THREE.MeshBasicMaterial({
-  map: texture,
-  // wireframe: true,
-});
-const globus = new THREE.Mesh(geometry, material);
-scene.add(globus);
-
-camera.position.z = 10;
-
-points.forEach((point, index) => {
-  const o = createObject(index + 1, 0x0000ff)
-  const {x, y, z} = calcPosFromLatLonRad(point[0], point[1]);
-  o.position.set(x, y, z);
-  scene.add(o)
-})
-
-function draw() {
-  window.requestAnimationFrame(draw);
-  renderer.render(scene, camera);
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
-draw();
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-function createObject(number, color) {
-  const objGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2,);
-  const objMaterial = new THREE.MeshBasicMaterial({color});
-  const object = new THREE.Mesh(objGeometry, objMaterial);
-  object.name = `object${number}`;
-  return object;
-}
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-function calcPosFromLatLonRad(lat, lon) {
-  var phi = (90 - lat) * (Math.PI / 180);
-  var theta = (lon + 180) * (Math.PI / 180);
-
-  let x = 5 * -(Math.sin(phi) * Math.cos(theta));
-  let z = 5 * Math.sin(phi) * Math.sin(theta);
-  let y = 5 * Math.cos(phi);
-
-  return {x, y, z, phi, theta};
-}
-
-const buttons = document.querySelectorAll('button');
-
-function onClickButton(e) {
-  const { id } = e.target;
-  console.log(id);
-}
-
-buttons.forEach((button) => {
-  button.addEventListener('click', onClickButton);
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
+
+const canvas = document.getElementById('canvas');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+const geometry = new THREE.BoxGeometry(1, 1,  1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  // wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const clock = new THREE.Clock();
+
+const mouseCoords = {
+  x: 0,
+  y: 0,
+}
+
+window.addEventListener('mousemove', (event) => {
+  mouseCoords.x = event.clientX / window.innerWidth - 0.5
+  mouseCoords.y = event.clientY / window.innerHeight - 0.5
+});
+
+
+function draw() {
+  const time = clock.getElapsedTime();
+
+  // mesh.rotation.y = time
+  // mesh.position.y = Math.abs(Math.sin(time * Math.PI));
+  // mesh.position.x = Math.cos(time * Math.PI);
+
+  camera.position.x = -mouseCoords.x;
+  camera.position.y = mouseCoords.y;
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(draw);
+}
+
+draw();
